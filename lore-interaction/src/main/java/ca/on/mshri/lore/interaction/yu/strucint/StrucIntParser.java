3fe3086ef66d1d69694232e59b36d13e60b88b5d
jochenweile <jochenweile@gmail.com>
2013-01-18 19:17:19 -0500

    refactored to use TabDelimParser helper

--- lore-interaction/src/main/java/ca/on/mshri/lore/interaction/yu/strucint/StrucIntParser.java
+++ lore-interaction/src/main/java/ca/on/mshri/lore/interaction/yu/strucint/StrucIntParser.java
@@ -23,142 +23,149 @@ import ca.on.mshri.lore.interaction.InteractionModel;
 import ca.on.mshri.lore.interaction.PhysicalInteraction;
 import ca.on.mshri.lore.molecules.Protein;
 import ca.on.mshri.lore.molecules.ProteinDomain;
-import ca.on.mshri.lore.operations.LoreOperation;
 import ca.on.mshri.lore.operations.util.Parameter;
+import ca.on.mshri.lore.operations.util.TabDelimParser;
 import ca.on.mshri.lore.operations.util.URLParameter;
 import com.hp.hpl.jena.ontology.OntClass;
 import com.hp.hpl.jena.ontology.OntModelSpec;
 import com.hp.hpl.jena.rdf.model.Property;
-import de.jweile.yogiutil.CliIndeterminateProgress;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
+ * Parses the structural interaction network from the Yu Lab (Wang et al 2012).
+ * 
  * @author Jochen Weile <jochenweile@gmail.com>
  */
-public class StrucIntParser extends LoreOperation {
+public class StrucIntParser extends TabDelimParser {
 
+    /**
+     * The source URL. Required!
+     */
     public final URLParameter srcP = new URLParameter("src");
+    
+    /**
+     * A string describing the experiment (defaults to Wang_2012)
+     */
     public final Parameter<String> experimentP = Parameter.make("experiment", String.class, "Wang_2012");
+    
+    /*
+     * Shared fields
+     */
+    private InteractionModel iaModel;
+    private OntClass physInt;
+    private Authority domAuth;
+    private Property involvedIn;
+    private Experiment exp;
+    
+    /*
+     * Column indices
+     */
+    private static final int proteinA = 0;
+    private static final int proteinB = 1;
+    private static final int pfamA = 2;
+    private static final int startA = 3;
+    private static final int endA = 4;
+    private static final int pfamB = 5;
+    private static final int startB = 6;
+    private static final int endB = 7;
     
     @Override
     public void run() {
         
         Logger.getLogger(StrucIntParser.class.getName()).log(Level.INFO, "Structural interactome parser started.");
         
-        InteractionModel model = new InteractionModel(OntModelSpec.OWL_MEM, getModel());
-        OntClass physInt = model.getOntClass(PhysicalInteraction.CLASS_URI);
-        Authority domAuth = Authority.createOrGet(model, "LoreProteinDomain");
+        //init fields
+        iaModel = new InteractionModel(OntModelSpec.OWL_MEM, getModel());
+        physInt = iaModel.getOntClass(PhysicalInteraction.CLASS_URI);
+        domAuth = Authority.createOrGet(iaModel, "LoreProteinDomain");
+        involvedIn = iaModel.getProperty(InteractionModel.URI+"#involvedIn");
+        exp = Experiment.createOrGet(iaModel, getParameterValue(experimentP));
         
-        Property involvedIn = model.getProperty(InteractionModel.URI+"#involvedIn");
-        
-        Experiment exp = Experiment.createOrGet(model, getParameterValue(experimentP));
-        
+        //check out source URL
         URL src = getParameterValue(srcP);
+        if (src == null) {
+            throw new IllegalArgumentException("Missing src parameter!");
+        }
         
-        int proteinA = 0;
-        int proteinB = 1;
-        int pfamA = 2;
-        int startA = 3;
-        int endA = 4;
-        int pfamB = 5;
-        int startB = 6;
-        int endB = 7;
-        
-        InputStream in = null;
+        //start parser
         try {
-            
-            in = src.openStream();
-            BufferedReader r = new BufferedReader(new InputStreamReader(in));
-            
-            CliIndeterminateProgress progress = new CliIndeterminateProgress();
-            
-            String line; int lnum = 0;
-            while ((line = r.readLine()) != null) {
-                lnum++;
-                
-                //skip header
-                if (lnum < 6) {
-                    progress.next("Parsing");
-                    continue;
-                }
-                
-                String[] cols = line.split("\t");
-                
-                if (cols.length < 8) {
-                    Logger.getLogger(StrucIntParser.class.getName())
-                            .log(Level.WARNING, "Invalid line: "+lnum);
-                    progress.next("Parsing");
-                    continue;
-                }
-                
-                Protein pA = Protein.createOrGet(model, model.ENTREZ, cols[proteinA]);
-                Protein pB = Protein.createOrGet(model, model.ENTREZ, cols[proteinB]);
-                
-                PhysicalInteraction interaction = PhysicalInteraction.createOrGet(model, exp, physInt, pA,pB);
-                
-                ProteinDomain domA = processDomain(model, domAuth, cols[proteinA], 
-                        cols[pfamA], pA, Integer.parseInt(cols[startA]), 
-                        Integer.parseInt(cols[endA]));
-                ProteinDomain domB = processDomain(model, domAuth, cols[proteinB], 
-                        cols[pfamB], pB, Integer.parseInt(cols[startB]), 
-                        Integer.parseInt(cols[endB]));
-                
-                domA.addProperty(involvedIn, interaction);
-                domB.addProperty(involvedIn, interaction);
-                
-                progress.next("Parsing");
-            }
-            
-            progress.done();
-            
+            parseTabDelim(src.openStream(), 6, 8);
         } catch (IOException ex) {
             throw new RuntimeException("Unable to read "+src, ex);
-        } finally {
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException ex) {
-                    Logger.getLogger(StrucIntParser.class.getName())
-                            .log(Level.WARNING, "Unable to close stream", ex);
-                }
-            }
         }
         
     }
 
+    /**
+     * Process tab-delimited row.
+     * @param cols Array of column values for this row.
+     */
     @Override
-    public boolean requiresReasoner() {
-        return false;
+    protected void processRow(String[] cols) {
+        
+        //build proteins
+        Protein pA = Protein.createOrGet(iaModel, iaModel.ENTREZ, cols[proteinA]);
+        Protein pB = Protein.createOrGet(iaModel, iaModel.ENTREZ, cols[proteinB]);
+
+        //build interaction between proteins
+        PhysicalInteraction interaction = PhysicalInteraction.createOrGet(iaModel, exp, physInt, pA,pB);
+
+        //build domains
+        ProteinDomain domA = makeDomain(cols[proteinA], cols[pfamA], pA, 
+                Integer.parseInt(cols[startA]), 
+                Integer.parseInt(cols[endA]));
+        ProteinDomain domB = makeDomain(cols[proteinB], cols[pfamB], pB, 
+                Integer.parseInt(cols[startB]), 
+                Integer.parseInt(cols[endB]));
+
+        //link domains with interaction
+        domA.addProperty(involvedIn, interaction);
+        domB.addProperty(involvedIn, interaction);
+        
     }
 
-    private ProteinDomain processDomain(InteractionModel model, Authority domAuth,  
-            String entrez, String pfam, Protein protein, int start, int end) {
+    /**
+     * Builds a ProteinDomain object from the given information
+     * @param entrez EntrezGene id
+     * @param pfam PFam id
+     * @param protein protein object to link with
+     * @param start start position on protein
+     * @param end end position on protein
+     * @return the ProteinDomain object.
+     */
+    private ProteinDomain makeDomain(String entrez, String pfam, Protein protein, int start, int end) {
         
+        //build identifier: ENTREZ.PFAM:START-END
         StringBuilder id = new StringBuilder();
         id.append(entrez).append(".").append(pfam).append(":").append(start).append("-").append(end);
         
-        ProteinDomain domain = ProteinDomain.createOrGet(model, domAuth, id.toString());
+        //create or retrive existing domain object
+        ProteinDomain domain = ProteinDomain.createOrGet(iaModel, domAuth, id.toString());
         
+        //if it's pre-existing
         if (domain.getProtein() != null) { 
-            //it's a pre-existing domain
+            //check consistency
             if (!domain.getProtein().equals(protein)) {
                 throw new InconsistencyException("Domain associated with multiple proteins!");
             } 
         } else {
-            //it's a new domain
-            domain.addXRef(model.PFAM, pfam);
+            //if it's a new object, init everything
+            domain.addXRef(iaModel.PFAM, pfam);
             domain.setProtein(protein);
             domain.setStart(start);
             domain.setEnd(end);
         }
+        
         return domain;
+    }
+
+    
+    @Override
+    public boolean requiresReasoner() {
+        return false;
     }
     
 }
