40452a1f2771bccda49e1c1d7af40c87c5ce5ac1
jochenweile <jochenweile@gmail.com>
2013-02-06 11:24:30 -0500

    eliminate "duplicate" orfs during parsing. also: javadoc

--- lore-interaction/src/main/java/ca/on/mshri/lore/interaction/edgotype/InteractionParserV2.java
+++ lore-interaction/src/main/java/ca/on/mshri/lore/interaction/edgotype/InteractionParserV2.java
@@ -32,18 +32,27 @@ import com.hp.hpl.jena.ontology.OntModelSpec;
 import com.hp.hpl.jena.rdf.model.Property;
 import java.io.IOException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
 /**
- *
+ * Parses version 2 of the edgotyping initiative file format
+ * 
  * @author Jochen Weile <jochenweile@gmail.com>
  */
 public class InteractionParserV2 extends TabDelimParser {
         
+    /**
+     * source URL from where to read the data.
+     */
     public URLParameter srcP = new URLParameter("src");
     
+    /**
+     * ID of the experiment that created the data.
+     */
     public Parameter<String> expP = Parameter.make("exp", String.class);
     
     //column indices
@@ -75,7 +84,15 @@ public class InteractionParserV2 extends TabDelimParser {
     private OntClass physIntType;
     private Property pos;
     private Property neg;
+    
+    /**
+     * Remember which genes were already handled.
+     */
+    private Map<String,String> gene2allele = new HashMap<String, String>();
         
+    /**
+     * Runs the parser.
+     */
     public void run() {
         
         Logger.getLogger(InteractionParserV2.class.getName())
@@ -107,47 +124,69 @@ public class InteractionParserV2 extends TabDelimParser {
         
     }
 
+    /**
+     * Processes a row from the tab delimted file.
+     * @param cols 
+     */
     @Override
     protected void processRow(String[] cols) {
         
+        //if DB gene or AD gene already have a different ORF, then skip this row
+        if (isDuplicate(cols, dbEntrezId, dbOrfId) 
+                || isDuplicate(cols, adEntrezId, adOrfId)) {
+            return;
+        }
+        
+        //Make DB gene and DB protein. Protein will be identified with the ORF id
         Gene dbGene = getOrMakeGene(cols, dbEntrezId, dbSymbol);
         Protein dbProtein = getOrMakeProtein(cols, dbGene, dbEntrezId, dbSymbol, dbOrfId);
 
+        //Make AD gene and AD protein. as above.
         Gene adGene = getOrMakeGene(cols, adEntrezId, adSymbol);
         Protein adProtein = getOrMakeProtein(cols, adGene, adEntrezId, adSymbol, adOrfId);
 
+        //Create the interaction between the proteins
+        PhysicalInteraction interaction = PhysicalInteraction
+                .createOrGet(iaModel, exp, physIntType, dbProtein, adProtein);
 
-        PhysicalInteraction interaction = PhysicalInteraction.createOrGet(iaModel, exp, physIntType, dbProtein, adProtein);
-
+        //Create the allele. Major alleles have URI=OrfID.0
+        //Minor alleles have URI=MutantId
         Allele dbAllele = Allele.createOrGet(iaModel, ccsbMut, 
                 cols[mutId].equals("0") ? 
-                cols[dbOrfId]+"."+cols[mutId] : 
+                cols[dbOrfId]+".0" : 
                 cols[mutId]
         );
+        //Connect allele to gene if not already done.
         if (dbAllele.getGene() == null || !dbAllele.getGene().equals(dbGene)) {
             dbAllele.setGene(dbGene);
         }
+        //Add HGMD ID to allele if not already done.
         if (dbAllele.getXRefValue(hgmd) == null) {
             dbAllele.addXRef(hgmd, cols[hgmdAcc]);
         }
+        //Add Mutation to Allele if not already done or Major allele.
         if (dbAllele.listMutations().isEmpty() && !cols[mutAAPos].equals("WT")) {
-            PointMutation pmut = PointMutation.createOrGet(iaModel, dbAllele, cols[mutAAPos]);
+            PointMutation pmut = PointMutation
+                    .createOrGet(iaModel, dbAllele, cols[mutAAPos]);
             dbAllele.addMutation(pmut);
         }
 
-
-        //complement short lines
-//        String key = (cols.length < score+1) ? "" : cols[score];
-
+        //Connect allele to interaction regarding positive or negative influece
         Growth growth = Growth.fromKey(cols[score]);
-
-        //register allele with interaction
         if (growth != Growth.UNKNOWN) {
             Property affects = growth != Growth.NEG ? pos : neg;
             dbAllele.addProperty(affects, interaction);
         }
     }
 
+    /**
+     * Get the gene object for the given IDs or make a new one if it doesn't 
+     * exist yet.
+     * @param cols the column array
+     * @param entrezIndex the array index for the entrez id
+     * @param symbolIndex the array index for the gene symbol
+     * @return the gene object.
+     */
     private Gene getOrMakeGene(String[] cols, int entrezIndex, int symbolIndex) {
         //get model components
         Gene gene = Gene.createOrGet(iaModel, iaModel.ENTREZ, cols[entrezIndex]);
@@ -157,7 +196,19 @@ public class InteractionParserV2 extends TabDelimParser {
         return gene;
     }
 
-    private Protein getOrMakeProtein(String[] cols, Gene gene, int entrezIndex, int symbolIndex, int orfIndex) {
+    /**
+     * Get the protein object for the given ID or make a new one if it doesn't
+     * exist yet. 
+     * @param cols The column array.
+     * @param gene Gene that encodes for this protein
+     * @param entrezIndex Array index for the entrez id
+     * @param symbolIndex Array index for gene symbol
+     * @param orfIndex Array index for ORF ID
+     * @return the protein object.
+     */
+    private Protein getOrMakeProtein(String[] cols, Gene gene, int entrezIndex, 
+            int symbolIndex, int orfIndex) {
+        
         Protein protein = Protein.createOrGet(iaModel, ccsbOrf, cols[orfIndex]);
         if (protein.getXRefValue(iaModel.HGNC) == null) {
             protein.addXRef(iaModel.HGNC, cols[symbolIndex]);
@@ -170,7 +221,31 @@ public class InteractionParserV2 extends TabDelimParser {
         }
         return protein;
     }
+
+    /**
+     * Returns whether another ORF already exists for this gene.
+     * @param cols column array
+     * @param entrezIndex array index for entrez id
+     * @param orfIndex array index for orf id
+     * @return 
+     */
+    private boolean isDuplicate(String[] cols, int entrezIndex, int orfIndex) {
+        
+        String entrezId = cols[entrezIndex];
+        String orfId = cols[orfIndex];
+        
+        String knownOrf = gene2allele.get(entrezId);
+        if (knownOrf == null || knownOrf.equals(orfId)) {
+            gene2allele.put(entrezId, orfId);
+            return false;
+        } else {
+            return true;
+        }
+    }
     
+    /**
+     * Enum for representing different experimental results
+     */
     private static enum Growth {
         //y, 0, y-,  y+,    aa
         POS,NEG,WEAK,STRONG,UNKNOWN;
@@ -190,6 +265,10 @@ public class InteractionParserV2 extends TabDelimParser {
         }
     }
 
+    /**
+     * does not require reasoner support.
+     * @return false
+     */
     @Override
     public boolean requiresReasoner() {
         return false;
