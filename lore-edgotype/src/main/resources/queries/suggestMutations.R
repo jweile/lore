4e7a63efda07a150ea5b4f89fa90649f5cd004f6
jochenweile <jochenweile@gmail.com>
2013-06-03 19:12:07 -0400

    documentation

--- lore-edgotype/src/main/resources/queries/suggestMutations.R
+++ lore-edgotype/src/main/resources/queries/suggestMutations.R
@@ -206,25 +206,20 @@ init.translator <- function(ctable.file="input/codontable.txt") {
 	list(translate=translate)	
 }
 
+##
+# Initialize translator
+#
 translator <- init.translator()
 
-# suggestMutations <- function(ncSeq, codons) {
 
-# 	ncs <- c('A','C','G','T')
-
-# 	for (i in seq(1,nchar(ncSeq),3)) {
-		
-# 		codon <- substr(ncSeq,i,i+2)
-# 		aa <- codons$getSingleForCodon(codon)
-
-# 		for (j in 1:3) {
-# 			nc <- substr(codon,j,j)
-# 			alternatives <- setdiff(ncs,nc)
-# 		}
-# 	}
-
-# }
-
+##
+# PCR simulation function
+# sequence = original DNA sequence, should start with a start codon and end with a stop codon
+# cycles = number of PCR cycles to simulate. Should be > 1, but too large numbers will affect runtime and memory usage exponentially
+# init.amount = Initial amount of template molecules to use in the simulation
+# etr = Enzyme-to-Template ratio. Defaults to 1/1
+# mutation.rate = Mutations per bp introduced by the enzyme per replication process.
+#
 pcr.sim <- function(sequence, cycles=10, init.amount=100, etr=1, mut.rate=1/2000) {
 
 	enzyme.amount <- round(init.amount * etr)
@@ -327,21 +322,68 @@ pcr.sim <- function(sequence, cycles=10, init.amount=100, etr=1, mut.rate=1/2000
 }
 
 
+
+
+##
+# Heatmap drawing function
+#
+hmap <- function(data, main, col=heat.colors(50)) {
+	mar.orig <- (par.orig <- par(c("mar", "las", "mfrow")))$mar
+    on.exit(par(par.orig))
+    w <- (3 + mar.orig[2]) * par("csi") * 2.54
+    layout(matrix(c(2, 1), ncol = 2), widths = c(1, lcm(w)))
+    par(las = 1)
+    mar <- mar.orig
+    mar[4] <- mar[2]
+    mar[2] <- 1
+    par(mar = mar) 
+
+    zlim <- range(data)
+    levels <- seq(zlim[1], zlim[2], length = length(col))
+    plot.new()
+    plot.window(xlim = c(0, 1), ylim = range(levels), xaxs = "i", yaxs = "i")
+    rect(0, levels[-length(levels)], 1, levels[-1], col = col,  density = NA)
+    axis(4)
+    box()
+    mar <- mar.orig
+    mar[4] <- 0
+    par(mar = mar)
+
+    par(las=0)
+	image(
+		data,
+		axes=FALSE,
+		xlab="#Cycles",
+		ylab="Enzyme/Template ratio",
+		main=main,
+		col=col
+	)
+	axis(1, at=seq(0,1,length.out=length(rownames(data))), labels=rownames(data))
+	axis(2, at=seq(0,1,length.out=length(colnames(data))), labels=colnames(data))
+
+}
+
+
+
+
+
+#####################
+#####################
+## START OF SCRIPT ##
+#####################
+#####################
+
+
+
+# DNA template to use
 template <- "ATGGCTGACCAACTGACTGAAGAGCAGATTGCAGAATTCAAAGAAGCTTTTTCACTATTTGACAAAGATGGTGATGGAACTATAACAACAAAGGAATTGGGAACTGTAATGAGATCTCTTGGGCAGAATCCCACAGAAGCAGAGTTACAGGACATGATTAATGAAGTAGATGCTGATGGTAATGGCACAATTGACTTCCCTGAATTTCTGACAATGATGGCAAGAAAAATGAAAGACACAGACAGTGAAGAAGAAATTAGAGAAGCATTCCGTGTGTTTGATAAGGATGGCAATGGCTATATTAGTGCTGCAGAACTTCGCCATGTGATGACAAACCTTGGAGAGAAGTTAACAGATGAAGAAGTTGATGAAATGATCAGGGAAGCAGATATTGATGGTGATGGTCAAGTAAACTATGAAGAGTTTGTACAAATGATGACAGCAAAGTGA"
 
-pcr.result <- pcr.sim(template)
-pcr.stats <- table(sapply(pcr.result, function(mutations) {
-	if (any(mutations == "truncation")) {
-		"truncation"
-	} else if (any(mutations == "nonsense")) {
-		"nonsense"
-	} else {
-		sum(mutations != "silent")
-	}
-}))
-
-
-
+##
+# Cycle over different parameter combinations
+# record yield, efficiency and share of singles in separate matrices
+#
+# WARNING: This takes a long time to execute!
+#
 
 cycle.vals <- 1:40
 ratio.vals <- 2^seq(-3,4,.2)
@@ -386,53 +428,22 @@ rownames(singleness.matrix) <- as.character(cycle.vals)
 colnames(singleness.matrix) <- as.character(ratio.vals)
 
 
-
-hmap <- function(data, main, col=heat.colors(50)) {
-	mar.orig <- (par.orig <- par(c("mar", "las", "mfrow")))$mar
-    on.exit(par(par.orig))
-    w <- (3 + mar.orig[2]) * par("csi") * 2.54
-    layout(matrix(c(2, 1), ncol = 2), widths = c(1, lcm(w)))
-    par(las = 1)
-    mar <- mar.orig
-    mar[4] <- mar[2]
-    mar[2] <- 1
-    par(mar = mar) 
-
-    zlim <- range(data)
-    levels <- seq(zlim[1], zlim[2], length = length(col))
-    plot.new()
-    plot.window(xlim = c(0, 1), ylim = range(levels), xaxs = "i", yaxs = "i")
-    rect(0, levels[-length(levels)], 1, levels[-1], col = col,  density = NA)
-    axis(4)
-    box()
-    mar <- mar.orig
-    mar[4] <- 0
-    par(mar = mar)
-
-    par(las=0)
-	image(
-		data,
-		axes=FALSE,
-		xlab="#Cycles",
-		ylab="Enzyme/Template ratio",
-		main=main,
-		col=col
-	)
-	axis(1, at=seq(0,1,length.out=length(rownames(data))), labels=rownames(data))
-	axis(2, at=seq(0,1,length.out=length(colnames(data))), labels=colnames(data))
-
-}
-
 hmap(efficiency.matrix, "Efficiency")
 hmap(yield.matrix, "Yield")
 hmap(log2(yield.matrix+1), "Log Yield")
 hmap(singleness.matrix,"Share of singles")
 
 
-#Colony picking
 
-pcr.result <- pcr.sim(template)
+##
+# Examine single result more closely
+#
 
+cycles <- 100
+etr <- 100
+pcr.result <- pcr.sim(template, cycles=cycles, etr=etr, mut.rate=1/1000)
+
+# A function to compute the distribution of mutation counts
 mut.distr <- function(mutations) table(sapply(mutations, function(mutations) {
 	if (any(mutations == "truncation")) {
 		"truncation"
@@ -443,11 +454,27 @@ mut.distr <- function(mutations) table(sapply(mutations, function(mutations) {
 	}
 }))
 
+#Draw distribution of mutations as bar plot
+op <- par(las=3)
+barplot(mut.distr(pcr.result),
+	main=paste("SAC distribution:",cycles,"cycles, ",etr,"enzymes/template"),
+	xlab="Mutations",
+	ylab="Frequency"
+)
+par(op)
 
-sample.size <- 2*96
 
+
+##
+# Simulate Colony picking
+#
+sample.size <- 20*96
+
+##
+# Perform the picking process 1000 times and collect results in matrix
+#
 categories <- names(mut.distr(pcr.result))
-stats <- apply(t(sapply(
+pickings <- apply(t(sapply(
 		sapply(1:1000,function(x) {
 			mut.distr(sample(pcr.result, sample.size, replace=TRUE))
 		}), 
@@ -455,21 +482,27 @@ stats <- apply(t(sapply(
 	)),
 	c(1,2), function(element) if (is.na(element)) 0 else element
 )
-colnames(stats) <- categories
+colnames(pickings) <- categories
 
+##
+# Draw a boxplot of distributions likely to result from picking
+#
 op<-par(las=3)
-boxplot(stats, col="orange",main=paste("Picked",sample.size), xlab="mutants", ylab="frequency")
+boxplot(pickings, col="orange",main=paste("Picked",sample.size), xlab="mutants", ylab="frequency")
 par(op)
 
 
-#Reachable amino acids
 
+####
+# Compute map of reachable and observed mutations
+####
 
-sample.size <- 10*96
-
+#translate DNA template to obtain protein sequence
 protein <- translator$translate(template)
+# a function that returns the i'th codon from the template
 codon.at <- function(dna, i) substr(dna,3*i-2, 3*i)
 
+#initialize a matrix covering all sequence positions times all possible amino acids
 change.matrix <- matrix(NA,
 	nrow=21,
 	ncol=nchar(protein),
@@ -478,6 +511,7 @@ change.matrix <- matrix(NA,
 		1:nchar(protein)
 	)
 )
+# Mark the fields corresponding to the original aminoacids in the matrix, as well as all potentially reachable mutations
 for (i in 1:nchar(protein)) {
 	codon <- codon.at(template,i)
 	for (pos in 1:3) {
@@ -491,11 +525,13 @@ for (i in 1:nchar(protein)) {
 	change.matrix[char.at(protein,i),i] <- -1
 }
 
+# Simulate colony picking and obtain list of mutations in those colonies
 pcr.sample <- sample(pcr.result, sample.size, replace=TRUE)
 pcr.sample.sacs <- unlist(pcr.sample[sapply(pcr.sample, {
 	function(x) length(x) > 0 && !any(x == "truncation" | x == "nonsense")
 })])
 
+# Mark mutations in the matrix
 for (sac in pcr.sample.sacs) {
 
 	if (sac == "silent") next
@@ -505,10 +541,14 @@ for (sac in pcr.sample.sacs) {
 	change.matrix[aa,pos] <- change.matrix[aa,pos] + 1
 }
 
+# Compute a color gradient to represent the mutation counts
 maxVal <- max(apply(change.matrix,1,function(x) max(na.omit(x))))
 colors <- colorRampPalette(c("white", "orange"))(maxVal+1)
 
+### Draw the diagram
+# set drawing color to gray and use horizontal axis labels
 op <- par(fg="gray",las=1)
+# create an empty plot
 plot(0,
 	type='n',
 	axes=FALSE,
@@ -518,27 +558,37 @@ plot(0,
 	ylab="Amino acid",
 	main=paste("SAC coverage for",sample.size,"colonies")
 )
+# iterate over each matrix entry and draw the contents on the plot
 for (x in 1:nchar(protein)) {
 	for (y in 1:21) {
 		if (!is.na(change.matrix[y,x])) {
 			if (change.matrix[y,x] > 0) {
+				#observed mutations are drawn in a color shade corresponding to their count
 				rect(x-1,22-y,x,21-y,col=colors[change.matrix[y,x]+1], lty="dotted")
 			} else if (change.matrix[y,x] == -1) {
+				#original amino acids are marked in gray
 				rect(x-1,22-y,x,21-y,col="gray")
 			} else {
+				#reachable aminoacids are marked with dotted outline
 				rect(x-1,22-y,x,21-y, lty="dotted")
 			}
 		}
 	}
 }
+# draw axes
 axis(1, at=(1:nchar(protein))-.5, labels=1:nchar(protein))
 axis(2, at=(1:21)-.5, labels=rev(rownames(change.matrix)) )
 par(op)
 
 
+##
+# General mutation coverage for each position
+#
 
+# compute how many of the reachable mutations are observed for each position
 coverage <- (apply(change.matrix,2,function(x) sum(na.omit(x) > 0)) 
 	/ apply(change.matrix,2,function(x) sum(!is.na(x))))
+# draw a bar plot for the above coverage
 barplot(coverage,
 	main=paste("SAC coverage for",sample.size,"colonies"), 
 	xlab="Position",
@@ -546,117 +596,3 @@ barplot(coverage,
 	ylim=c(0,1),
 	border=NA
 )
-
-
-# ##
-# # Simulates mutagenic PCR experiment on a given amount of DNA molecules with 
-# # given sequence for the number of given PCR cycles, with the given enzyme/template ratio (etr)
-# mutagenesis <- function(seq, cycles=10, init.amount=100, etr=1, mut.rate=1/2000) {
-
-# 	#Build transition matrix according to MutazymeII manual
-# 	mut <- cbind(
-# 		rbind(
-# 			A=c(A=0,C=.047,G=.175,T=.285),
-# 			C=c(A=.141,C=0,G=.041,T=.255),
-# 			G=c(A=.255,C=.041,G=0,T=.141),
-# 			T=c(A=.285,C=.175,G=.047,T=0)
-# 		) * .5,
-# 		DEL=rep(.048,4)/4,
-# 		INS=rep(.008,4)/4
-# 	) * 4 * mut.rate
-# 	for (i in 1:4) {
-# 		mut[i,i] <- 1-sum(mut[i,])
-# 	}
-# 	#Transform to cumulative matrix
-# 	cmut <- cbind(mut[,1],sapply(2:ncol(mut), function(i) apply(mut[,1:i],1,sum)))
-# 	dimnames(cmut) <- dimnames(mut)
-
-# 	#potentially mutates the given nucleotide
-# 	mutate <- function(nucleotide) {
-# 		r <- runif(1,0,1)
-# 		mutation <- names(which.min(which(r < cmut[nucleotide,])))
-# 		if (mutation == "DEL") {
-# 			""
-# 		} else if (mutation == "INS") {
-# 			paste(nucleotide, sample(c('A','C','G','T'),1), sep="")
-# 		} else {
-# 			mutation
-# 		}
-# 	}
-
-# 	enzyme.amount <- round(init.amount * etr)
-
-# 	dna <- rep(seq, init.amount)
-
-# 	for (c in 1:cycles) {
-# 		dna <- c(dna, 
-# 			sapply(sample(dna, min(length(dna),enzyme.amount)), function(curr.template) {
-# 				paste(sapply(to.char.array(curr.template), mutate),collapse="")
-# 			})
-# 		)
-# 	}
-
-# 	names(dna) <- NULL
-
-# 	dna[-(1:init.amount)]
-
-# }
-
-# classify.muts <- function(template, mutants) {
-# 	sapply(mutants, function(mut) {
-# 		if (nchar(template) != nchar(mut)) {
-# 			"truncation"
-# 		} else {
-# 			sum(sapply(1:nchar(template), function(i) char.at(template,i) != char.at(mut,i)))
-# 		}
-# 	})
-# }
-
-# find.snps <- function(template, mut, align.cutoff=5) {
-# 	if (nchar(template) == nchar(mut)) {
-
-# 		snps <- unlist(sapply(1:nchar(template), function(i) {
-# 			if (char.at(template,i) == char.at(mut,i)) NULL else paste(char.at(template,i),i,char.at(mut,i),sep="")
-# 		}))
-
-# 		# if (length(snps) > align.cutoff) {
-# 		# 	al <- new.alignment(template, mut)
-# 		# 	al$getMutations()
-# 		# } else {
-# 		# 	snps
-# 		# }
-
-# 		snps
-
-# 	} else {
-
-# 		al <- new.alignment(template, mut)
-# 		al$getMutations()
-
-# 	}
-# }
-
-
-# template <- "ATGGCTGACCAACTGACTGAAGAGCAGATTGCAGAATTCAAAGAAGCTTTTTCACTATTTGACAAAGATGGTGATGGAACTATAACAACAAAGGAATTGGGAACTGTAATGAGATCTCTTGGGCAGAATCCCACAGAAGCAGAGTTACAGGACATGATTAATGAAGTAGATGCTGATGGTAATGGCACAATTGACTTCCCTGAATTTCTGACAATGATGGCAAGAAAAATGAAAGACACAGACAGTGAAGAAGAAATTAGAGAAGCATTCCGTGTGTTTGATAAGGATGGCAATGGCTATATTAGTGCTGCAGAACTTCGCCATGTGATGACAAACCTTGGAGAGAAGTTAACAGATGAAGAAGTTGATGAAATGATCAGGGAAGCAGATATTGATGGTGATGGTCAAGTAAACTATGAAGAGTTTGTACAAATGATGACAGCAAAGTGA"
-# templ.protein <- translator$translate(template)
-
-# dna <- mutagenesis(template, init.amount=20, etr=4)
-
-# proteins <- sapply(dna, translator$translate)
-
-# num.prot.muts <- classify.muts(templ.protein, proteins)
-# num.dna.muts <- sapply(dna, function(mut) {
-# 	length(find.snps(template,mut))
-# })
-
-# op <- par(mfrow=c(1,2))
-# barplot(table(num.dna.muts),xlab="Mutations",ylab="Frequency",main="DNA level")
-# barplot(table(num.prot.muts),xlab="Mutations",ylab="Frequency",main="Protein level")
-# par(op)
-
-# # num.muts <- sapply(proteins[1:100], function(mutant) {
-# # 	al <- new.alignment(templ.protein, mutant)
-# # 	al$getDistance()
-# # })
-
-
